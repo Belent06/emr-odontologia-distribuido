57e891d551987ec45889586668404d49c6efce62
Belent06 <belentashiguano@gmail.com>
2026-01-17 12:28:33 -0500

    feat: mongodb persistence for svc-history complete

--- apps/svc-history/src/app/app.controller.ts
+++ apps/svc-history/src/app/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @EventPattern('patient_created')
-  handlePatientCreated(@Payload() data: any) {
-    console.log('✅ [HISTORY-SVC] Evento recibido!');
+  async handlePatientCreated(@Payload() data: any) {
+    console.log('✅ [HISTORY-SVC] Evento de RabbitMQ recibido correctamente');
     console.log('📦 Datos completos del paciente:', data);
+    await this.appService.createInitialHistory(data);
   }
 }
--- apps/svc-history/src/app/app.module.ts
+++ apps/svc-history/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {
+  PatientHistory,
+  PatientHistorySchema,
+} from './schemas/patient-history.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://admin:adminpassword@localhost:27017/history_db?authSource=admin',
+    ),
+    // 👇 REGISTRAMOS EL MODELO AQUÍ 👇
+    MongooseModule.forFeature([
+      { name: PatientHistory.name, schema: PatientHistorySchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
