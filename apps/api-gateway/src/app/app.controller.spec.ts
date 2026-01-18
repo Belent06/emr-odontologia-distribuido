92ab80dc208232f6e7d8809cf29a4666a45f72f6
Belent06 <belentashiguano@gmail.com>
2026-01-17 23:24:51 -0500

    fix(gateway): update init tests with http and history service

--- apps/api-gateway/src/app/app.controller.spec.ts
+++ apps/api-gateway/src/app/app.controller.spec.ts
@@ -1,21 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
+import { of } from 'rxjs';
 
 describe('AppController', () => {
-  let app: TestingModule;
+  let appController: AppController;
 
-  beforeAll(async () => {
-    app = await Test.createTestingModule({
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        // 👇 NECESITAMOS LOS MISMOS MOCKS AQUÍ PORQUE EL SERVICIO LOS PIDE
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(() => of({ data: [] })),
+            post: jest.fn(() => of({ data: {} })),
+          },
+        },
+        {
+          provide: 'HISTORY_SERVICE',
+          useValue: {
+            send: jest.fn(() => of([])),
+            emit: jest.fn(),
+          },
+        },
+      ],
     }).compile();
+
+    appController = module.get<AppController>(AppController);
   });
 
-  describe('getData', () => {
-    it('should return "Hello API"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Hello API' });
-    });
+  it('should be defined', () => {
+    expect(appController).toBeDefined();
   });
 });
